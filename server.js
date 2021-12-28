2ed10840e1a68b24c5ba4698bd4930158674f94c
Juan De Ponte <52869895+DePonteJuan@users.noreply.github.com>
2021-12-28 18:55:37 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const http = require("http")
+const fs = require("fs")
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+    //set header content Type
+    res.setHeader('Content-Type', 'text/html')
+    //setting up the path depending on the client's url
+    let path = "./view/"
+    switch(req.url) {
+        case '/':
+            path+='index.html';
+            res.statusCode = 200;
+            break
+        case '/about':
+            path+='about.html';
+            res.statusCode = 200;
+            break
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location','/about')
+            res.end()
+            break
+        default:
+            path+='404.html';
+            res.statusCode = 404;
+            break
+    }
+    // send an html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end()
+        }
+        //res.write(data);
+        res.end(data)
+    })
+})
+
+server.listen(3000, 'localhost', () =>{
+    console.log('listening to requests on port 3000')
+})
